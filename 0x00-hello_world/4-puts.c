c404a9c8e140c5511b8b1482a69c8cf63b763490
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-13 04:08:48 -0700

    Issue with puts function fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
+ *
+ *
  * main - Main Function
+ *
  * Return: Return 0 if successful
+ *
+ *
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
 }
 
