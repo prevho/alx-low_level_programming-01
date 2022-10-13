d3caef6bd6266557639c5fb599a885c2a4c7ca7e
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-13 02:12:47 -0700

    I just wrote another quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function to write a quote
+ * Return: Return 1 if successful
+ */
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, q, 60);
+	return (1);
+}
+
