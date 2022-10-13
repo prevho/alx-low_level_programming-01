e149b530de8aa3f097cd44e862fefc9e22e71b30
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-13 01:51:24 -0700

    I just put the quote

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ * Return: Return 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
