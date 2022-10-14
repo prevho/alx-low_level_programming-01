ce0fa33bcde3dc12e25a818014929dbd60006041
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-14 04:21:55 -0700

    Fixed betty issue with space

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char a;
 
-	for (a = 'a'; a<= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
-	for (a = 'A'; a<= 'Z'; a++)
+	for (a = 'A'; a <= 'Z'; a++)
 		putchar(a);
 	putchar('\n');
 
