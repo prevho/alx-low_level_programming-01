701a87fe97463a12c23861be57a41313c37eb7e3
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-18 02:26:30 -0700

    Why print alphabet x10?

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - prints alphabets x10
+ * Return: returns zero ar the end
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	_putchar('\n');
+	}
+
+	return (0);
+}
