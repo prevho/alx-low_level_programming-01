38e74e6477d55aab6943cac72c0abb4597e3d602
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 02:09:43 -0700

    Now we are printing underscores

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Prints the digit
+ *
+ * Return: last digit.
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
