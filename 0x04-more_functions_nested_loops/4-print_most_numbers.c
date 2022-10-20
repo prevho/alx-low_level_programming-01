7c54306ffa494e82592128a95d39f9b055974b8e
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 01:11:58 -0700

    Apologies to 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints most numbers
+ * Return: last digit.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if(i == 2 || i ==4)
+        		continue;
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
