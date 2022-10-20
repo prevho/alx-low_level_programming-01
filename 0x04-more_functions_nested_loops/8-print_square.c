3fb2ce20aa90b6f9c75d9ef38b9e5b975f380a2b
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 04:52:41 -0700

    Updated some codes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < size; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 ==0))
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
