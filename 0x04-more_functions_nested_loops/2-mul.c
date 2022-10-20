09816060adffae69e4708e1e807d8fa2fa661ef7
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 00:25:59 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Prints the digit
+ *
+ * @a: input number as an integer.
+ * @b: input number as an integer.
+ *
+ * Return: last digit.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
