0fce36854fac93b3c1af990aa147c39ed8304db1
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 04:45:54 -0700

    Header file updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the digit
- *
- * Return: last digit.
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 
 void more_numbers(void)
 {
@@ -12,18 +10,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
-			{
+			if (j > 9)
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			_putchar((j % 10) + '0');
 		}
-	_putchar('\n');
+
+		_putchar('\n');
 	}
 }
