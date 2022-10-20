0f805d7cd9bee81d1fa376fdea1190565d0dc5cf
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 04:17:54 -0700

    Header file updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the digit
- * Return: last digit.
- */
+* print_numbers - function that prints the numbers, from 0 to 9.
+*/
 
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+
 	_putchar('\n');
 }
