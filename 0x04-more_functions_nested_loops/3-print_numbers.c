e3c8b6277bc3c14cfa53a0c67a934d8621e7d80c
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 01:01:22 -0700

    Numbers shall be printed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the digit
+ *
+ *
+ * Return: last digit.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
