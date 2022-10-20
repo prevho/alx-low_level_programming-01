7633a79c32f5538cb8dea8e7970d59c6d96f322f
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-19 23:27:01 -0700

    Lowercase issue

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * * _islower - Retuns 1 if char is lowercase character
- * * else, return 0
- * *
- * * @c: The character in ASCII code
- * *
- * * Return: 1 for lowercase. 0 for the rest.
- * */
+ * _islower - Retuns 1 if char is lowercase character
+ * else, return 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase. 0 for the rest.
+ */
 
 int _islower(int c)
 {
