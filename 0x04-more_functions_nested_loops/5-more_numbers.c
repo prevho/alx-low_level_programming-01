0366e59a5bf6b6a8ec1f7ee2bbc1c66f8b5ef74f
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 01:47:54 -0700

    For the love of more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the digit
+ *
+ * Return: last digit.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+				_putchar(j + '0');
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
