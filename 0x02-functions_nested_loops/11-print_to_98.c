3e33b2a019cca87bb434e8bd41207f14a8159e17
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-20 20:13:13 -0700

    Space problems

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,17 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-	        for (; n > 98; n--)
-	        {
-	                printf("%d, ", n);
-	        }
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
 	else if (n < 98)
 	{
-	        for (; n < 98; n++)
-	        {
-	                printf("%d, ", n);
-	        }
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
 	}
 	printf("%d\n", n);
 }
