53fa794f4b32c6e3d5dfaa5690a498140727423d
Precious Iriaevho <precious.iriaevho@gmail.com>
2022-10-28 18:34:26 -0700

    Main Header is king

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_number(int n);
+*infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
